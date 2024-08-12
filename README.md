0b5019df05d5c1bb7f7e5ea5f013a8cd493ec380
HouDeCloud <418130444@qq.com>
2024-08-12 15:45:11 +0800

    update

--- README.md
+++ README.md
@@ -3,7 +3,12 @@
 官方原版仓库: https://github.com/hiyouga/LLaMA-Factory
 
 ## 调试
-- 最终可用版本
+### 1 调试 cli.py
+>命令
+```sh
+FORCE_TORCHRUN=1 llamafactory-cli train examples/train_full/llama3_full_sft_ds3.yaml
+```
+
 >launch.json
 ```json
 {
@@ -46,3 +51,56 @@ sys.argv = ["llamafactory-cli", "train", "examples/train_full/llama3_full_sft_ds
 if __name__ == "__main__":
     main()
 ```
+
+### 2 调试 launcher.py
+>命令
+- 从 1 获得
+```sh
+torchrun --nnodes 1 --node_rank 0 --nproc_per_node 1 \
+  --master_addr 127.0.0.1 --master_port 20570 \
+  /root/datav/nlp/LLama-Factory-annotations/src/llamafactory/launcher.py \
+  examples/train_full/llama3_full_sft_ds3.yaml
+```
+
+>launch.json
+- 使用 `which torchrun` 找到 torchrun 路径
+- 然后直接把断点打在 launcher.py 里面就可以了
+```json
+{
+    // Use IntelliSense to learn about possible attributes.
+    // Hover to view descriptions of existing attributes.
+    // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
+    
+    "version": "0.2.0",
+    "configurations": [
+        
+        {
+            "name": "Python: Current File",
+            "type": "debugpy",
+            "request": "launch",
+            // "module": "llamafactory.cli",
+            "program": "/opt/miniconda/envs/llama39/bin/torchrun",  // torchrun 的完整路径
+            "args": [
+                "--nnodes", "1",
+                "--node_rank", "0",
+                "--nproc_per_node", "1",
+                "--master_addr", "127.0.0.1",
+                "--master_port", "20570",
+                "/root/datav/nlp/LLama-Factory-annotations/src/llamafactory/launcher.py",  // 脚本路径
+                "examples/train_full/llama3_full_sft_ds3.yaml" 
+            ],
+            "cwd": "${workspaceFolder}",
+            "env": {
+                // "FORCE_TORCHRUN": "true",
+                "PATH": "/opt/miniconda/envs/llama39/bin:${env:PATH}" 
+            },
+            "subProcess": true,
+            "console": "integratedTerminal",
+            "justMyCode": false // 允许调试库代码
+        }
+    ]
+}
+```
+
+
+### 3 调试 
--- launch.sh
+++ launch.sh
@@ -1,32 +1,38 @@
 # FORCE_TORCHRUN=1 llamafactory-cli train examples/train_full/llama3_full_sft_ds3.yaml
 
 
-python -m torch.distributed.launch \
-  --nproc_per_node 8 \
-  ./src/train.py \
-    --deepspeed ./examples/deepspeed/ds_z3_config.json \
-    --stage sft \
-    --do_train \
-    --model_name_or_path /home/ma-user/work/01440561/01449344/output \
-    --dataset_dir ./data \
-    --dataset identity \
-    --template qwen \
-    --finetuning_type full \
-    --output_dir ./output  \
-    --overwrite_cache true \
-    --overwrite_output_dir true \
-    --cutoff_len 1024 \
-    --preprocessing_num_workers 16 \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 32 \
-    --lr_scheduler_type cosine \
-    --logging_steps 1 \
-    --warmup_ratio 0.1 \
-    --learning_rate 5e-5 \
-    --num_train_epochs 5.0 \
-    --plot_loss \
-    --bf16 \
-    --val_size 0.1 \
-    --eval_steps 3 \
-    --evaluation_strategy steps
+# python -m torch.distributed.launch \
+#   --nproc_per_node 8 \
+#   ./src/train.py \
+#     --deepspeed ./examples/deepspeed/ds_z3_config.json \
+#     --stage sft \
+#     --do_train \
+#     --model_name_or_path /home/ma-user/work/01440561/01449344/output \
+#     --dataset_dir ./data \
+#     --dataset identity \
+#     --template qwen \
+#     --finetuning_type full \
+#     --output_dir ./output  \
+#     --overwrite_cache true \
+#     --overwrite_output_dir true \
+#     --cutoff_len 1024 \
+#     --preprocessing_num_workers 16 \
+#     --per_device_train_batch_size 4 \
+#     --per_device_eval_batch_size 4 \
+#     --gradient_accumulation_steps 32 \
+#     --lr_scheduler_type cosine \
+#     --logging_steps 1 \
+#     --warmup_ratio 0.1 \
+#     --learning_rate 5e-5 \
+#     --num_train_epochs 5.0 \
+#     --plot_loss \
+#     --bf16 \
+#     --val_size 0.1 \
+#     --eval_steps 3 \
+#     --evaluation_strategy steps
+
+
+torchrun --nnodes 1 --node_rank 0 --nproc_per_node 1 \
+  --master_addr 127.0.0.1 --master_port 20570 \
+  /root/datav/nlp/LLama-Factory-annotations/src/llamafactory/launcher.py \
+  examples/train_full/llama3_full_sft_ds3.yaml
